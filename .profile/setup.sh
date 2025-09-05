7b7f7d6aa781d9d09e02b966b3283d8d8c7d601c
philipwlewis <philip.lewis@ucl.ac.uk>
2025-09-05 15:07:47 +0000

    changes

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -1,12 +1,13 @@
 #!bash
+for PROFILE in "/workspaces/*/.profile"; do
 
-REPO=/workspaces/*/.profile || exit 1
-
-echo RUNNING SETUP 
-
-if [ -f /workspaces/UoE-Python/.profile/bashrc ]; then
+echo RUNNING SETUP for $REPO
+if [ -f $PROFILE/bashrc ]; then
+   echo SETTING UP BASHRC
    mv ~/.bashrc ~/.bashrc.bk
    mv ~/.bash_history ~/.bash_history.bk || echo no hist
-   ln -s /workspaces/$REPO/.profile/bashrc ~/.bashrc || echo FAILED_LINK_BASHRC
-   ln -s /workspaces/$REPO/.profile/bash_history ~/.bash_history || echo FAILED_LINK_BASH_HISTORY
+   ln -s $PROFILE/bashrc ~/.bashrc || echo FAILED_LINK_BASHRC
+   ln -s $PROFILE/.profile/bash_history ~/.bash_history || echo FAILED_LINK_BASH_HISTORY
 fi
+
+done
