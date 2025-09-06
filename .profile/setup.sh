354b06d4d3f2dee5c57faa95b1d2755089e22107
philipwlewis <philip.lewis@ucl.ac.uk>
2025-09-06 07:32:42 +0100

    Update setup.sh

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -4,8 +4,8 @@ for PROFILE in "/workspaces/*/.profile"; do
 echo RUNNING SETUP for $REPO
 if [ -f $PROFILE/bashrc ]; then
    echo SETTING UP BASHRC
-   mv ~/.bashrc ~/.bashrc.bk
-   mv ~/.bash_history ~/.bash_history.bk || echo no hist
+   mv ~/.bashrc ~/.bashrc.bk || echo previous bashrc
+   mv ~/.bash_history ~/.bash_history.bk || echo no previous hist
    ln -s $PROFILE/bashrc ~/.bashrc || echo FAILED_LINK_BASHRC
    ln -s $PROFILE/bash_history ~/.bash_history || echo FAILED_LINK_BASH_HISTORY
 fi
