3d5efed52a05f66fd3c5d31b48a3052cf6b4edea
philipwlewis <philip.lewis@ucl.ac.uk>
2025-09-06 07:27:00 +0100

    Update setup.sh

--- .profile/setup.sh
+++ .profile/setup.sh
@@ -7,7 +7,7 @@ if [ -f $PROFILE/bashrc ]; then
    mv ~/.bashrc ~/.bashrc.bk
    mv ~/.bash_history ~/.bash_history.bk || echo no hist
    ln -s $PROFILE/bashrc ~/.bashrc || echo FAILED_LINK_BASHRC
-   ln -s $PROFILE/.profile/bash_history ~/.bash_history || echo FAILED_LINK_BASH_HISTORY
+   ln -s $PROFILE/bash_history ~/.bash_history || echo FAILED_LINK_BASH_HISTORY
 fi
 
 done
